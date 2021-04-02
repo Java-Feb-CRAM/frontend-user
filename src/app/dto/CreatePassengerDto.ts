f1ca3e84d56595d6d5d1945861466a98caff6537
Rob Maes <robert.maes@smoothstack.com>
2021-04-02 13:57:42 -0500

    UT-48 create basic add to cart->view cart->checkout flow

---
+++ src/app/dto/CreatePassengerDto.ts
@@ -0,0 +1,7 @@
+export interface CreatePassengerDto {
+  givenName: string;
+  familyName: string;
+  dob: string;
+  gender: string;
+  address: string;
+}
--- src/app/services/booking.service.ts
+++ src/app/services/booking.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { CreateGuestBookingDto } from '../dto/CreateGuestBookingDto';
+import { Booking } from '../models/Booking';
 
 @Injectable({
   providedIn: 'root',
 })
 export class BookingService {
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  guestBookingUrl = 'http://localhost:8083/bookings/guest';
+
+  createGuestBooking(
+    createGuestBookingDto: CreateGuestBookingDto
+  ): Observable<Booking> {
+    return this.http.post<Booking>(this.guestBookingUrl, createGuestBookingDto);
+  }
 }
